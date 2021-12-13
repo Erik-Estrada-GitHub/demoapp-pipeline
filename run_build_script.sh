fd9aa25b755177e8c597c2c5a0af329ef381cbfb
Erik-Estrada-GitHub <95895581+Erik-Estrada-GitHub@users.noreply.github.com>
2021-12-12 20:54:02 -0600

    Update run_build_script.sh
    Commit para cambiar la descripcion de lo que hace este script

--- run_build_script.sh
+++ run_build_script.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-echo "Run build script for demo pipeline"
+echo "Erik. This is my BUILD stage: Run build script for demo pipeline"
+echo "este script imprime un mensaje y hace un touch target/demoapp.jar"
 touch target/demoapp.jar
